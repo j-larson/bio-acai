966bb3f2c681a560550ca3981395dddf09099c8f
Ben Darwin <bcdarwin@gmail.com>
2014-06-30 11:43:50 -0400

    switch to affine gap algorithm (but with linear gap penalty) since it returns both score and alignment

--- Main.hs
+++ Main.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveDataTypeable, RecordWildCards, StandaloneDeriving #-}
 
 import Bio.Alignment.AlignData                  (toStrings)
-import Bio.Alignment.SAlign             as A
+import Bio.Alignment.AAlign             as A
 import Bio.Alignment.Matrices           as M
 import Bio.Sequence
 import Bio.Sequence.Fasta               as F
@@ -27,17 +27,16 @@ data Args = Args { dataDir  :: String
                  , minFrac  :: Double
                  , minScore :: Int
                  , linkage  :: Linkage
-                 -- , linkage  :: LinkageOpt
                  }
             deriving (Show, Data, Typeable)
 
--- TODO bring in Data.Default ??
 opts :: Args
-opts  = Args { dataDir  = "." &= opt "."  -- ??? redundant; how to show the defaults automatically in help?
-                              &= help "Directory to search for .faa files; default ."
-             , minFrac  = 0.0 &= opt (0.0 :: Double) -- ???
-             , minScore = 50  &= opt (50  :: Int)    -- ???
-             , linkage  = SingleLinkage &= opt SingleLinkage }     -- ???
+opts  = Args { dataDir  = def &= opt "." &= typ "DIR"
+                 &= help "Directory to search for .faa files; default \".\""
+             -- how to show the defaults automatically in usage message?
+             , minFrac  = def &= opt (0.0 :: Double)
+             , minScore = def &= opt (50  :: Int)      -- ???
+             , linkage  = SingleLinkage &= opt SingleLinkage }
              &= summary "Sequence clustering based on local alignment scoring"
 
 ident :: AnnSeq a -> String
@@ -49,8 +48,8 @@ ident s = seqFile s ++ ":" ++ show (seqNum s)
 infinity :: Double
 infinity  = 1/0.0
 
-gapPen :: Int
-gapPen  = -5
+gapPen :: (Int, Int)
+gapPen  = (-5,-5)
 
 trim :: FilePath -> FilePath
 trim  = reverse . drop 4 . reverse
@@ -63,17 +62,15 @@ main  = withFile "clustering" WriteMode $ \cfile ->
         withFile "matrix"     WriteMode $ \mfile -> do
   Args {..}  <- cmdArgs opts
   fs <- map trim . filter (".faa" `isSuffixOf`) <$> getDirectoryContents dataDir
-  seqss <- mapM (\f -> zipWith (AnnSeq f) [1..] `fmap` F.readFasta (untrim f)) fs
+  seqss <- mapM (\f -> zipWith (AnnSeq f) [1..] <$> F.readFasta (untrim f)) fs
   let d x y
-        | frac >= minFrac =
-        -- this recomputation of the score is spurious as we know the alignment
-        -- so we can find a nicer way of extracting it:
-            1.0 / fromIntegral (A.local_score M.blosum62 gapPen x y)
+        | frac >= minFrac = 1.0 / fromIntegral score
         | otherwise = infinity
         where
-          frac    = fromIntegral tot / fromIntegral (length s1)
-          tot     = sum (zipWith (\a b -> fromEnum (a == b && a /= '-')) s1 s2)
-          (s1,s2) = toStrings $ A.local_align M.blosum62 gapPen x y
+          frac = fromIntegral tot / fromIntegral (length s1)
+          tot  = sum (zipWith (\a b -> fromEnum (a == b && a /= '-')) s1 s2)
+          (s1,s2)      = toStrings seqs
+          (score,seqs) = A.local_align M.blosum62 gapPen x y
       clusters = zip [1::Int ..] $
           C.dendrogram linkage (concat seqss) (d `on` seqData)
         `cutAt` (1.0 / fromIntegral minScore)
