be155c44c99c0f4445011a0619cfa857cfc074f2
j-larson <johan.g.larson@gmail.com>
2014-07-01 07:18:11 -0400

    verify output files do not already exist

--- Main.hs
+++ Main.hs
@@ -12,7 +12,8 @@ import Data.Data
 import Data.Function                            (on)
 import Data.List                                (intercalate, isSuffixOf)
 import System.Console.CmdArgs
-import System.Directory                         (getDirectoryContents)
+import System.Directory
+import System.Exit
 import System.IO
 
 -- type of sequences annotated with source file information
@@ -67,23 +68,54 @@ d minFrac x y
     (s1,s2)      = toStrings seqs
     (score,seqs) = A.local_align M.blosum62 gapPen x y
 
+-- Error out if a file or directory exists at the given path.
+verifyNonExistent :: FilePath -> IO ()
+verifyNonExistent path = do
+  fileExists <- doesFileExist path
+  when fileExists $ do
+    putStrLn $ "ERROR: file \"" ++ path ++ "\" already exists"
+    exitFailure
+  directoryExists <- doesDirectoryExist path
+  when directoryExists $ do
+    putStrLn $ "ERROR: directory \"" ++ path ++ "\" already exists"
+    exitFailure
+
 main :: IO ()
-main  = withFile "clustering" WriteMode $ \cfile ->
-        withFile "matrix"     WriteMode $ \mfile -> do
+main  = do
   Args {..}  <- cmdArgs opts
+
+  let clustFileName = "clustering"
+      matrixFileName = "matrix"
+  mapM_ verifyNonExistent [clustFileName, matrixFileName]
+
+  print dataDir
+  print minFrac
+
+  -- get the input files
   fs <- map trim . filter (".faa" `isSuffixOf`)
           <$> getDirectoryContents dataDir
+  when (null fs) $ do
+    putStrLn $ "ERROR: directory " ++ dataDir ++ " contains no .faa input files"
+    exitFailure
+
+  -- run the main computation
   seqss <- mapM (\f -> zipWith (AnnSeq f) [1..] <$> F.readFasta (untrim f)) fs
   let clusters = zip [1::Int ..] $
           C.dendrogram linkage (concat seqss) (d minFrac `on` seqData)
         `cutAt` (1.0 / fromIntegral minScore)
 
+  -- create the clusters file
+  cfile <- openFile clustFileName WriteMode
   forM_ clusters $ \(n,c) ->
     hPutStrLn cfile $
       "cluster_" ++ show n ++ " " ++ intercalate "," (map ident (elements c))
+  hClose cfile
 
+  -- create the matrix file
+  mfile <- openFile matrixFileName WriteMode
   hPutStrLn mfile $ "," ++ intercalate "," fs
   forM_ clusters $ \(n,c) -> do
     hPutStr   mfile $ "cluster_" ++ show n ++ " "
     hPutStrLn mfile $ intercalate "," $
       map (show . fromEnum . (`elem` map seqFile (elements c))) fs
+  hClose mfile
